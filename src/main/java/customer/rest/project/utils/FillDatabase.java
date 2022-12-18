239e6b95384872908896ecb3a5ad55b520e9aab8
Jesse Plym <bgu254@myy.haaga-helia.fi>
2022-12-18 18:36:47 +0200

    test data added

--- src/main/java/customer/rest/project/utils/FillDatabase.java
+++ src/main/java/customer/rest/project/utils/FillDatabase.java
@@ -27,18 +27,40 @@ public class FillDatabase {
 		cRepo.deleteAll();
 		tRepo.deleteAll();
 
-		Customer cust1 = new Customer("Jesse", "Plymander", "Kirvuntie", "02600", "Espoo", "j.p@mail.com",
+		Customer cust1 = new Customer("Jesse", "Plymander", "Kirvuntie 1", "02600", "Espoo", "j.p@mail.com",
 				"0451101127");
-		Customer cust2 = new Customer("Tikru", "Kissanen", "Asematie", "02700", "Kauniainen", "t.k@mail.com",
+		Customer cust2 = new Customer("Tikru", "Kissanen", "Asematie 1", "02700", "Kauniainen", "t.k@mail.com",
 				"0451101128");
+		Customer cust3 = new Customer("Aliisa", "Gröna", "Puolakantie 19", "00540", "Helsinki", "a.g@mail.com",
+				"0451101129");
+		Customer cust4 = new Customer("Matti", "Meikäläinen", "Rauhankatu 82", "00840", "Helsinki", "m.m@mail.com",
+				"0451101120");
+		Customer cust5 = new Customer("Maija", "Meikäläinen", "Kluuvikatu 85", "01510", "Helsinki", "m.ma@mail.com",
+				"0451101121");
+		Customer cust6 = new Customer("Paula", "Mäkinen", "Piilostentie 64", "18100", "Heinola", "p.m@mail.com",
+				"0451101122");
+		Customer cust7 = new Customer("Paavo", "Nieminen", "Kartanomäenkatu 93", "78300", "Varkaus", "p.n@mail.com",
+				"0451101123");
+		Customer cust8 = new Customer("Karru", "Haddudi", "Pohjoisesplanadi 47", "00210", "Helsinki", "k.h@mail.com",
+				"0451101124");
+		Customer cust9 = new Customer("Mamo", "Haddudi", "Pohjoisesplanadi 47", "00210", "Helsinki", "m.h@mail.com",
+				"0451101125");
+		Customer cust10 = new Customer("Erkki", "Erakko", "Rautatienkatu 42", "88600", "Sotkamo", "e.e@mail.com",
+				"0451101126");
 
 		// save all customers to repository
-		cRepo.saveAll(Arrays.asList(cust1, cust2));
+		cRepo.saveAll(Arrays.asList(cust1, cust2, cust3, cust4, cust5, cust6, cust7, cust8, cust9, cust10));
 
 		Training train1 = new Training(LocalDateTime.now(), 60, "Running", cust1);
+		Training train2 = new Training(LocalDateTime.now().plusDays(1), 45, "Fitness", cust3);
+		Training train3 = new Training(LocalDateTime.now().plusDays(2), 90, "Jogging", cust6);
+		Training train4 = new Training(LocalDateTime.now().plusDays(3), 20, "HIIT", cust4);
+		Training train5 = new Training(LocalDateTime.now().plusDays(1), 75, "Gym", cust1);
+		Training train6 = new Training(LocalDateTime.now().plusDays(4), 25, "HIIT", cust7);
+		Training train7 = new Training(LocalDateTime.now().plusDays(5), 50, "Fishing", cust10);
 
 		// save all training with customer to repository
-		tRepo.saveAll(Arrays.asList(train1));
+		tRepo.saveAll(Arrays.asList(train1, train2, train3, train4, train5, train6, train7));
 
 	}
 
