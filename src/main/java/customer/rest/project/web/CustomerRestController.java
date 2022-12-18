929d2a57ae413a34a250ae81d9c32d6c90861bca
Jesse Plym <bgu254@myy.haaga-helia.fi>
2022-12-18 18:59:35 +0200

    customer rest controller started

--- src/main/java/customer/rest/project/web/CustomerRestController.java
+++ src/main/java/customer/rest/project/web/CustomerRestController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +23,34 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	// get all customers
 	@GetMapping("/customers")
 	public @ResponseBody List<Customer> getCustomers() {
 		return (List<Customer>) customerRepository.findAll();
+	}
+
+	// get single customer
+	@GetMapping("/customers/{id}")
+	public @ResponseBody Customer getSingleCustomer(@PathVariable(name = "id") Long id) {
+		return (Customer) customerRepository.findById(id).get();
+	}
+
+	// save new customers
+	@PostMapping("/customers")
+	public void saveCustomers(@RequestBody List<Customer> customers) {
+		customerRepository.saveAll(customers);
+	}
+
+	// delete one customer
+	@DeleteMapping("/customers/{id}")
+	public void deleteCustomer(@PathVariable(name = "id") Long id) {
+		customerRepository.deleteById(id);
+	}
+
+	// TODO
+	@PutMapping("/customers/{id}")
+	public void editCustomer(@PathVariable(name = "id") Long id, @RequestBody Customer customer) {
+		customerRepository.save(customer);
 	}
 
 }
